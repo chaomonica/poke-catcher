9b73e5716a64455e73195650ebb3424812c0ac5f
chaomonica <alittlefaun@gmail.com>
2020-12-16 14:46:54 -0600

    seeded database

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const pgp = require('pg-promise')();
 
 require('dotenv').config();
 
-var connectionString = `postgres://postgres:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DBPORT}/${process.env.DATABASE}`;
+var connectionString = process.env.DATABASE_URL || `postgres://postgres:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DBPORT}/${process.env.DATABASE}`;
 
 const db = pgp(connectionString);
 
@@ -10,8 +10,8 @@ let generatePokemon = function(request, response) {
   if(!request.query.hasOwnProperty('number')) {
     response.send("generating a pokemon for you....")
   } else {
-    let pokemonId = request.query.number;
-
+    let pokemonId = 43;
+    //request.query.number
     db.any(`SELECT * FROM pokemon WHERE number=${pokemonId}`)
     .then((pokemonData)=>{
       response.send(pokemonData);
--- database/schema.sql
+++ database/schema.sql
@@ -25,3 +25,22 @@ COPY pokemon(number, name, catchMax, fleeMax, regURL)
 FROM '/home/monica/Documents/HackReactor/Week10/PokeCatcher/database/pokemon.csv'
 DELIMITER ','
 CSV HEADER;
+
+INSERT INTO
+    pokemon(number, name, catchMax, fleeMax, regURL)
+VALUES
+    (1, 'bulbasaur', 5, 10, 'https://img.pokemondb.net/artwork/large/bulbasaur.jpg'),
+    (2, 'ivysaur', 7, 7, 'https://img.pokemondb.net/artwork/large/ivysaur.jpg'),
+    (3, 'venusaur', 10, 5, 'https://img.pokemondb.net/artwork/large/venusaur.jpg'),
+    (4, 'charmander', 5, 10, 'https://img.pokemondb.net/artwork/large/charmander.jpg'),
+    (5, 'charmeleon', 7, 7, 'https://img.pokemondb.net/artwork/large/charmeleon.jpg'),
+    (6, 'charizard', 10, 5, 'https://img.pokemondb.net/artwork/large/charizard.jpg'),
+    (7, 'squirtle', 5, 10, 'https://img.pokemondb.net/artwork/large/squirtle.jpg'),
+    (8, 'wartortle', 7, 7, 'https://img.pokemondb.net/artwork/large/wartortle.jpg'),
+    (9, 'blastoise', 10, 5, 'https://img.pokemondb.net/artwork/large/blastoise.jpg');
+
+
+INSERT INTO
+    pokemon(number, name, catchMax, fleeMax, regURL)
+VALUES
+    (43, 'oddish', 5, 10, 'https://img.pokemondb.net/artwork/large/oddish.jpg');
